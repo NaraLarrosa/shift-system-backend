ca652533da19610d133341625abe18c4221c340d
NaraLarrosa <127156518+NaraLarrosa@users.noreply.github.com>
2023-12-12 11:27:18 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user-routes');
+const doctorRoutes = require('./routes/doctor-routes');
+const specialtyRoutes = require('./routes/specialty-routes');
+const shiftRoutes = require('./routes/shift-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// app.use(cors({ origin: true }));
+
+app.use('/api/user', userRoutes);
+app.use('/api/doctor', doctorRoutes);
+app.use('/api/specialty', specialtyRoutes);
+app.use('/api/shift', shiftRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find the given path.', 404);
+  throw error;
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  `mongodb+srv://nara:hola1234@cluster0.xkfnw3f.mongodb.net/shift-system?retryWrites=true&w=majority`
+)
+.then(() => {
+  app.listen(5000);
+})
+.catch(err => {
+  console.log(err);
+});
